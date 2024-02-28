3a6fbb70ac2edadcc1d991747017a2d0feb7d411
Sadia <150430383+sadiasaleem187@users.noreply.github.com>
2024-02-28 11:52:03 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 import inquirer from "inquirer";
-async function main() {
-    let { task } = await inquirer.prompt([{
+let todos = [];
+let loop = true;
+while (loop) {
+    let answer = await inquirer.prompt([
+        {
             type: "input",
-            name: "task",
-            message: "what you want to add"
-        }]);
-    console.log(`added task: ${task}`);
+            name: "TODO",
+            message: "What do you want to add in your todo?"
+        }, {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more todo?",
+            default: false
+        }
+    ]);
+    let { TODO, addmore } = answer;
+    loop = addmore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("Kindly add valid input");
+    }
 }
-main();
+if (todos.length > 0) {
+    console.log("Your Todo List is:");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No todos found");
+}
